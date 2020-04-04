0bb132e77e492ea1820853465f9d09906e42bfd1
Ubuntu <abrarsim2017@Ubuntu16.n0zp0vj2gzne5jqtjojikfwpig.gx.internal.cloudapp.net>
2020-04-04 04:06:28 +0000

    Edited text

---
+++ python/howdy_name.py
@@ -0,0 +1,5 @@
+first_name = input('Enter First Name: ')
+middle_name = input('Enter Middle Name: ')
+last_name = input('Enter Last Name: ')
+print()
+print('Howdy,',first_name,middle_name,last_name + '!')
